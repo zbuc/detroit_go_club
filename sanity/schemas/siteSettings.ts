02570516c93e419980827b8730ae514531d978e5
Chris Czub <chris@penumbralabs.xyz>
2025-08-28 00:31:54 -0400

    Remove Site Description field

--- sanity/schemas/siteSettings.ts
+++ sanity/schemas/siteSettings.ts
@@ -12,11 +12,6 @@ export default defineType({
       initialValue: 'Detroit Go Club',
     }),
     defineField({
-      name: 'description',
-      title: 'Site Description',
-      type: 'text',
-    }),
-    defineField({
       name: 'welcomeMessage',
       title: 'Welcome Message',
       type: 'array',
